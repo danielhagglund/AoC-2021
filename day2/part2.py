08299c656364ae0e69ccd4477b35d041da3f1c54
Daniel Hägglund <danihagl@gmail.com>
2021-12-02 20:00:26 +0000

    Day2

---
+++ day2/part2.py
@@ -0,0 +1,21 @@
+input_data = open("./course.txt").read().splitlines()
+
+course_steps = []
+final_course = {"horizontal": 0, "depth": 0, "aim": 0}
+
+for line in input_data:
+    step = line.split(' ')
+    course_steps.append({"direction": step[0], "distance": int(step[1]) })
+
+for step in course_steps:
+    if step["direction"] == "down":
+        final_course["aim"] = final_course["aim"] + step["distance"]
+    elif step["direction"] == "forward":
+        final_course["horizontal"] = final_course["horizontal"] + step["distance"]
+        final_course["depth"] = final_course["depth"] + (step["distance"] * final_course["aim"] )
+    elif step["direction"] == "up":
+        final_course["aim"] = final_course["aim"] - step["distance"]
+    
+
+print(f"Total horizontal: {final_course['horizontal']}, total depth: {final_course['depth']}")
+print("Product:", final_course["depth"]*final_course["horizontal"])
